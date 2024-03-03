010b877eb1203b3a7a17b32b7a9a6c53747128f0
Bat <bat@man.pl>
2024-03-04 00:17:09 +0100

    ADD :: DokumentacjaScrum.odt

---
+++ Shop/src/main/java/com/example/Shop/Accounts/Account.java
@@ -0,0 +1,13 @@
+package com.example.Shop.Accounts;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Account {
+
+    https://www.codejava.net/frameworks/spring-boot/user-registration-and-login-tutorial
+    https://docs.spring.io/spring-security/reference/servlet/authentication/passwords/index.html
+    https://spring.io/guides/gs/securing-web
+    https://www.javaguides.net/2018/10/user-registration-module-using-springboot-springmvc-springsecurity-hibernate5-thymeleaf-mysql.html
+
+}
